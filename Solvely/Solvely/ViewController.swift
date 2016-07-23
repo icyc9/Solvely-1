05539f3d06a538d1f2e6f3b6a4e1f77beb7d62f2
Daniel Christopher <dan_pc@live.com>
2016-07-23 00:45:14 -0400

    Completed bottom bars

--- Solvely/Solvely/ViewController.swift
+++ Solvely/Solvely/ViewController.swift
@@ -11,12 +11,23 @@ import CameraEngine
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var questionView: UIView!
+    
+    @IBOutlet weak var answerView: UIView!
+    
     private let cameraEngine = CameraEngine()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.cameraEngine.captureDevice
         self.cameraEngine.startSession()
+        
+        self.questionView.layer.cornerRadius = 10
+        
+        self.answerView.layer.cornerRadius = 10
+        self.answerView.layer.borderColor = UIColor.whiteColor().CGColor
+        self.answerView.layer.borderWidth = 4
+        self.answerView.layer.masksToBounds = true
     }
 
     override func viewDidLayoutSubviews() {
